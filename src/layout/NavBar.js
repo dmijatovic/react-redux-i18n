506a58ce9e9891051357dea92622f7e9f6de2928
Dusan Mijatovic (PC2016) <d.mijatovic@dv4all.nl>
2018-11-04 19:12:27 +0100

    translate route links does not work with selected flag?

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-
+//REDUX
+//import { connect } from 'react-redux';
+//ROUTER
 import { NavLink  } from 'react-router-dom';
+//LOCAL
 import { Clock, LanguageButtons } from '../component';
 import './NavBar.scss';
 import routes from '../router/routes';
-
+/**
+ * Navbar loads links from router and 2 nav widgets:
+ * LanguageButtons and Clock (not i18n)
+ */
 class NavBar extends Component {
   createLinks = () => {
     let links=[];
     routes.forEach( item => {
       if (item.label){
+        //extract translations
+        /*let trans
+        if (this.props.routeLabel){
+          trans = this.props.routeLabel[item.path];
+        }*/
         links.push(
           <li className="link-item"
             key={item.path}>
@@ -18,7 +29,7 @@ class NavBar extends Component {
               key={item.path}
               to={item.path}
               activeClassName="selected">
-                {item.label}
+                { item.label }
             </NavLink>
           </li>
         )
@@ -46,3 +57,25 @@ class NavBar extends Component {
 }
 
 export default NavBar;
+
+/* problems with router selected flag when using redux ?!?
+//------------------- REDUX CONNECTION --------------------------
+const mapStateToProps = state => {
+  //debugger
+  //get translations from i18n reducer
+  let { data } = state.i18n.lang;
+  if (data){
+    return {
+      routeLabel: data["NavBar.routeLabel"]
+    }
+  }else{
+    return {
+      routeLabel: null
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(NavBar);
+*/
